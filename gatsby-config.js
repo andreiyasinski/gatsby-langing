e18fc7bc307a632b19bc64327fe9cf2561928283
Andrei Yasinski <andrewtopol856@gmail.com>
2019-11-06 21:54:34 +0300

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,15 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ]
 }
